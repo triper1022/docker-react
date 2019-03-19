b0c4d3ac290c48e63b6e983661ef46666423501b
marco <triper1022@gmail.com>
2019-03-20 01:16:46 +0900

    added new words to app

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            hello it's changed. ##Edit <code>src/App.js</code> and save to reload.
+            hello it's changed again on feature branch. ##Edit <code>src/App.js</code> and save to reload.
           </p>
           <a
             className="App-link"
